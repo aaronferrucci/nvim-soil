5be6bba18964b9d7f7ae3ff2e851ac985b02d41d
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2025-05-13 14:10:58 -0700

    Update soil.lua
    s/Soil/soil/

--- plugin/soil.lua
+++ plugin/soil.lua
@@ -4,5 +4,5 @@ end
 
 vim.g.soil = 1
 
-vim.api.nvim_create_user_command('Soil', 'lua require("soil.core").run()', {})
+vim.api.nvim_create_user_command('soil', 'lua require("soil.core").run()', {})
 vim.api.nvim_create_user_command('SoilOpenImg', 'lua require("soil.core").open_image()', {})
